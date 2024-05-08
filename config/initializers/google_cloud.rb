5d2ddf5732fbf1eed8b5604cd3830e54b13776be
Darumai <darumaidev@gmail.com>
2024-05-07 21:29:26 -0300

    Added production clause to Google Cloud initializer

--- config/initializers/google_cloud.rb
+++ config/initializers/google_cloud.rb
@@ -1,6 +1,10 @@
-storage = Google::Cloud::Storage.new(
-  project_id: ENV["GOOGLE_CLOUD_PROJECT_ID"],
-  credentials: 'config/gcp.json'
-)
+require "google/cloud/storage"
 
-GOOGLE_STORAGE_BUCKET = storage.bucket(ENV["GOOGLE_STORAGE_BUCKET_NAME"])
+if Rails.env.production?
+  storage = Google::Cloud::Storage.new(
+    project_id: ENV["GOOGLE_CLOUD_PROJECT_ID"],
+    credentials: 'config/gcp.json'
+  )
+  
+  GOOGLE_STORAGE_BUCKET = storage.bucket(ENV["GOOGLE_STORAGE_BUCKET_NAME"])
+end
